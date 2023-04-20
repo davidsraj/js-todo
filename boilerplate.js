b5d517ba9bdf8251c59fdb6283ff1683c307a552
sivakumarrfourbends <sivakumar@fourbends.com>
2023-04-20 18:13:32 +0530

    jstodo-update using class

--- boilerplate.js
+++ boilerplate.js
@@ -1,80 +1,104 @@
-// Boilerplate: initialize the db
-(function () {
+class Todo {
   config = {
     locateFile: (filename) =>
       "https://cdn.jsdelivr.net/npm/sql.js@1.8.0/dist/sql-wasm.wasm",
   };
-  initSqlJs(config).then(function (SQL) {
-    // get db from local storage
-    var db = null;
-    localforage
-      .getItem("db")
-      .then(function (value) {
-        if (value) {
-          // if db exists, load it
-          db = new SQL.Database(value);
-        } else {
-          // if db doesn't exist, create it
-          console.log("Creating db");
-          db = new SQL.Database();
-          // Run a query without reading the results
-          db.run(
-            "CREATE TABLE todo (todo_id INTEGER PRIMARY KEY, todo TEXT NOT NULL, state TEXT NOT NULL);"
-          );
-        }
-        window.db = db;
-        renderTableData();
-      })
-      .catch(function (err) {
-        console.log("Error: " + err);
-      });
-  });
-})();
+  db;
+  insertQuery = "INSERT INTO todo VALUES (?, ?, ?)";
+  deleteQuery = "DELETE FROM todo WHERE todo_id = ?";
+  selectQuery = "SELECT * FROM todo";
+  createQuery =
+    "CREATE TABLE todo (todo_id INTEGER PRIMARY KEY, todo TEXT NOT NULL, state TEXT NOT NULL)";
 
-function renderTableData() {
-  // execute select
-  const result = db.exec("SELECT * FROM todo");
-  // construct table
-  // base case - no data
-  console.log(result);
-  if (result.length === 0) {
-    document.getElementById("sql-result").innerHTML = "";
-    return;
+  constructor() {
+    this.getLocalStorageData();
   }
 
-  // construct the header
-  let headerString = "";
-  result[0].columns.forEach((column) => {
-    headerString += `<th>${column}</th>`;
-  });
-  headerString += `<th>Actions</th>`;
-  headerString = `<tr>${headerString}</tr>`;
-
-  // construct the rows
-  var rows = "";
-  result[0].values.forEach((row) => {
-    var rowString = "";
-    row.forEach((column) => {
-      rowString += `<td>${column}</td>`;
+  getLocalStorageData() {
+    initSqlJs(this.config).then((SQL) => {
+      localforage
+        .getItem("db")
+        .then((value) => {
+          this.initDB(value, SQL);
+        })
+        .catch(function (err) {
+          console.log("Error: " + err);
+        });
     });
-    rowString += `<td><button class="btn btn-primary" onclick="deleteTODO(${row[0]})">Delete</button></td>`;
-    // rowString += `<td><button class="btn btn-primary">Delete</button></td>`;
-    rowString = `<tr>${rowString}</tr>`;
-    rows += rowString;
-  });
+  }
 
-  // render the table
-  let tableString = `<table>${headerString}${rows}</table>`;
+  initDB(value, SQL) {
+    if (value) {
+      // if db exists, load it
+      this.db = new SQL.Database(value);
+    } else {
+      // if db doesn't exist, create it
+      console.log("Creating db");
+      this.db = new SQL.Database();
+      // Run a query without reading the results
+      this.db.run(this.createQuery);
+    }
+    window.db = this.db;
+    this.renderTableData();
+  }
 
-  // insert the tableString into the table
-  document.getElementById("sql-result").innerHTML = tableString;
+  renderTableData() {
+    // execute select
+
+    const result = this.db.exec(this.selectQuery);
+    // construct table
+    // base case - no data
+    console.log(result, "result");
+    if (result.length === 0) {
+      document.getElementById("sql-result").innerHTML = "";
+      return;
+    }
+
+    // construct the header
+    let headerString = "";
+    result[0].columns.forEach((column) => {
+      headerString += `<th>${column}</th>`;
+    });
+    headerString += `<th>Actions</th>`;
+    headerString = `<tr>${headerString}</tr>`;
+
+    var rows = "";
+    result[0].values.forEach((row) => {
+      var rowString = "";
+      row.forEach((column) => {
+        rowString += `<td>${column}</td>`;
+      });
+      rowString += `<td><button class="btn btn-primary" onclick="deleteTODO(${row[0]})">Delete</button></td>`;
+
+      rowString = `<tr>${rowString}</tr>`;
+      rows += rowString;
+    });
+
+    let tableString = `<table>${headerString}${rows}</table>`;
+    document.getElementById("sql-result").innerHTML = tableString;
+    console.log("successfullyrendered");
+  }
+
+  renderTable() {
+    localforage.setItem("db", this.db.export()).catch(function (err) {
+      if (err) console.log(err);
+    });
+
+    this.renderTableData();
+  }
+
+  insertTODO() {
+    var text = document.getElementById("myInput").value;
+    this.db.run(this.insertQuery, [null, text, "Done"]);
+    console.log(this.db, "jjhj");
+    // Boilerplate: render the table
+    this.renderTable();
+  }
+
+  deleteTODO(todoID) {
+    this.db.run(this.deleteQuery, [todoID]);
+    this.renderTable();
+  }
 }
 
-function renderTable() {
-  // get the db and store it in local storage
-  localforage.setItem("db", db.export()).catch(function (err) {
-    if (err) console.log(err);
-  });
-
-  renderTableData();
-}
+const todo = new Todo();
--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 function insertTODO() {
-  var text = document.getElementById("myInput").value;
-  db.run("INSERT INTO todo VALUES (?, ?, ?)", [null, text, "Done"]);
-
-  // Boilerplate: render the table
-  renderTable();
+  todo.insertTODO();
 }
 
 function deleteTODO(todoID) {
-    db.run("DELETE FROM todo WHERE todo_id = ?", [todoID]);
-    
-    // Boilerplate: render the table
-    renderTable();
+  todo.deleteTODO(todoID);
 }
 
 // TODO this is more involved.. e.g. how to update a specific row?
 function updateTODO() {
-    var text = document.getElementById("myInput").value;
-    db.run("UPDATE todo SET state = 'Done' WHERE todo = ?", [text]);
-    
-    // Boilerplate: render the table
-    renderTable();
+  var text = document.getElementById("myInput").value;
+  db.run("UPDATE todo SET state = 'Done' WHERE todo = ?", [text]);
+
+  // Boilerplate: render the table
+  todo.renderTable();
 }
